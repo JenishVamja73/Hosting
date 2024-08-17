83a8b77c56a8b35801856ec3eeaf464c54b891f4
JenishVamja73 <jenishvamja091@gmail.com>
2024-08-17 19:30:15 +0530

    React-native

--- index.js
+++ index.js
@@ -5,14 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import analytics from '@react-native-firebase/analytics';
 import { enableScreens } from 'react-native-screens';
 enableScreens();
-
-
-analytics().logEvent('your_event_name', {
-    key1: 'value1',
-    key2: 'value2',
-  });
-
 AppRegistry.registerComponent(appName, () => App);
