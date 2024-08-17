ae50ed7f07f13acd9848d56f16c7c11a2d1f2b69
JenishVamja73 <jenishvamja091@gmail.com>
2024-08-17 09:24:39 +0530

    FirstREact-native

---
+++ screen/Navgtion/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const Navgtion =()=>{
+return(
+    <>
+    <View>
+        <Text>{"Heelo"}</Text>
+    </View>
+    </>
+)
+}
+export default Navgtion
